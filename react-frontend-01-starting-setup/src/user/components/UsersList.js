7a72e8333e1168ad80f9406c95b0f6955882cfa7
CWilliamsJusticeReskill <ChrisW@SccColorado.org>
2021-12-11 18:16:19 -0700

    Pushing up Styling App & Components

--- react-frontend-01-starting-setup/src/user/components/UsersList.js
+++ react-frontend-01-starting-setup/src/user/components/UsersList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import UserItem from "./UserItem";
+
 import "./UsersList.css";
 
 const UsersList = (props) => {
@@ -11,10 +13,16 @@ const UsersList = (props) => {
     );
   }
   return (
-    <ul>
-      {props.items.map(user => 
-      <UserItem />;
-      })}
+    <ul className="users-list">
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
     </ul>
   );
 };
--- react-frontend-01-starting-setup/src/user/pages/Users.js
+++ react-frontend-01-starting-setup/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Justice Reskill",
+      image:
+        "	https://justicereskill.com/_next/image?url=%2Fjustice-reskill-logo.png&w=1080&q=75",
+      places: 1,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;

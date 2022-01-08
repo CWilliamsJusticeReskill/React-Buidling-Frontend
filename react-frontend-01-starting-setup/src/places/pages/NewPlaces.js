cd788283d7e9844ffebec9a550f65cd030fe9e0f
CWilliamsJusticeReskill <ChrisW@SccColorado.org>
2022-01-07 23:26:21 -0700

    Pushing up form and Input additions

--- react-frontend-01-starting-setup/src/places/pages/NewPlaces.js
+++ react-frontend-01-starting-setup/src/places/pages/NewPlaces.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../../shared/components/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/components/util/validators";
 import "./NewPlace.css";
 
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      return {};
+    default:
+      return state;
+  }
+};
+
 const NewPlace = () => {
+  useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
@@ -13,6 +42,16 @@ const NewPlace = () => {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid Title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        type="text"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
